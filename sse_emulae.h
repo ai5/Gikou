eb817c8833ff9c1f31149fc3a5471fa6372bc990
ai5 <taibarax@gmail.com>
2016-06-02 23:10:24 +0900

    mingw 32bit用だったけどmingwはmutexが使えないので不要だけど一応コミット

--- sse_emulae.h
+++ sse_emulae.h
@@ -7,6 +7,20 @@
 // SSEPlusから必要なものをコピペしたもの
 // http://developer.amd.com/tools-and-sdks/archive/legacy-cpu-gpu-tools/sseplus-project/
 
+typedef union
+{
+	__m128i m128i;
+	__m64	m64[2];
+	uint64_t u64[2];
+	int64_t  s64[2];
+	uint32_t u32[4];
+	int32_t  s32[4];
+	uint16_t u16[8];
+	int16_t  s16[8];
+	uint8_t  u8[16];
+	int8_t   s8[16];
+} sse_m128;
+
 #ifdef _MSC_VER
 #include <emmintrin.h> // SSE2.0
 #ifdef _WIN64
@@ -20,7 +34,12 @@ __inline __m128i _mm_movpi64_epi64(__m64 __A)
 #ifdef USE_SSE42
 
 #ifndef _WIN64
-#define _mm_extract_epi64 _MM_EXTRACT_EPI64
+ #ifdef _MSC_VER
+  #define _mm_extract_epi64 _MM_EXTRACT_EPI64
+ #else
+  #define _mm_extract_epi64 _MM_EXTRACT_EPI64
+  #define _mm_insert_epi64 _MM_INSERT_EPI64
+ #endif
 #endif
 
 #else
@@ -35,10 +54,10 @@ __inline __m128i _mm_movpi64_epi64(__m64 __A)
 #ifdef _mm_testc_si128
 __inline int _MM_TESTZ_SI128(__m128i a, __m128i b)
 {
-	__m128i t;
-	t = _mm_and_si128(a, b);
-	t = _mm_packs_epi32(t, _mm_setzero_si128());
-	return t.m128i_u64[0] == 0;
+	sse_m128 t;
+	t.m128i = _mm_and_si128(a, b);
+	t.m128i = _mm_packs_epi32(t.m128i, _mm_setzero_si128());
+	return t.u64[0] == 0;
 }
 #endif
 
@@ -53,27 +72,27 @@ __inline int _MM_TESTC_SI128(__m128i a, __m128i b)
 #ifdef _mm_extract_epi64
 __inline __int64 _MM_EXTRACT_EPI64(__m128i a, const int ndx)
 {
-	__m128i mask;
+	sse_m128 mask;
 	switch (ndx & 0x1)
 	{
 	case 1:  a = _mm_srli_si128(a, 8); break;
 	}
 
-	mask = _mm_set_epi32(0x00000000, 0x00000000, 0xFFFFFFFF, 0xFFFFFFFF);
-	mask = _mm_and_si128(mask, a);
+	mask.m128i = _mm_set_epi32(0x00000000, 0x00000000, 0xFFFFFFFF, 0xFFFFFFFF);
+	mask.m128i = _mm_and_si128(mask.m128i, a);
 
-	return mask.m128i_u64[0];
+	return mask.u64[0];
 }
 #endif
 
 #ifdef _mm_insert_epi64
 __inline __m128i _MM_INSERT_EPI64(__m128i a, __int64 b, const int ndx)
 {
-	__m128i A;
-	A = a;
+	sse_m128 A;
+	A.m128i = a;
 
-	A.m128i_u64[ndx & 0x1] = b;
-	return A;
+	A.u64[ndx & 0x1] = b;
+	return A.m128i;
 }
 #endif
 
@@ -90,18 +109,23 @@ __inline __m128i _MM_CVTEPU8_EPI16(__m128i a)
 #define MIN( sd, s) (sd<s)?sd:s;
 __inline __m128i _MM_MIN_EPU16(__m128i a, __m128i b)
 {
-	__m128i temp;
+	sse_m128 temp;
+	sse_m128 A;
+	sse_m128 B;
+	
+	A.m128i = a;
+	B.m128i = b;
 
-	temp.m128i_u16[0] = MIN(a.m128i_u16[0], b.m128i_u16[0]);
-	temp.m128i_u16[1] = MIN(a.m128i_u16[1], b.m128i_u16[1]);
-	temp.m128i_u16[2] = MIN(a.m128i_u16[2], b.m128i_u16[2]);
-	temp.m128i_u16[3] = MIN(a.m128i_u16[3], b.m128i_u16[3]);
-	temp.m128i_u16[4] = MIN(a.m128i_u16[4], b.m128i_u16[4]);
-	temp.m128i_u16[5] = MIN(a.m128i_u16[5], b.m128i_u16[5]);
-	temp.m128i_u16[6] = MIN(a.m128i_u16[6], b.m128i_u16[6]);
-	temp.m128i_u16[7] = MIN(a.m128i_u16[7], b.m128i_u16[7]);
+	temp.u16[0] = MIN(A.u16[0], B.u16[0]);
+	temp.u16[1] = MIN(A.u16[1], B.u16[1]);
+	temp.u16[2] = MIN(A.u16[2], B.u16[2]);
+	temp.u16[3] = MIN(A.u16[3], B.u16[3]);
+	temp.u16[4] = MIN(A.u16[4], B.u16[4]);
+	temp.u16[5] = MIN(A.u16[5], B.u16[5]);
+	temp.u16[6] = MIN(A.u16[6], B.u16[6]);
+	temp.u16[7] = MIN(A.u16[7], B.u16[7]);
 
-	return temp;
+	return temp.m128i;
 }
 #endif
 
